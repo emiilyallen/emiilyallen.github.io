643ac25d161b936aa1afe7ce2000f4c775c5e537
emiilyallen <150135847+emiilyallen@users.noreply.github.com>
2024-12-03 09:53:54 +0000

    Pequeñas modificaciones

--- UT04-01/script.js
+++ UT04-01/script.js
@@ -1,3 +1,28 @@
+//DOM static selector
+const DOM = {
+    frm:document.getElementById("frm"),
+    nombre:document.getElementById("nombre"),
+    titulo:document.getElementById("titulo"),
+}
+
+
+(function(){ 
+    DOM.frm.addEventListener("submit", (e) => {
+        e.preventDefault();  // Usa punto y coma, no coma
+        alert(DOM.titulo.validationMessage);  // Usa punto y coma, no coma
+    })
+})()
+
+
+// if (!nombre.validationMessage==""){
+
+// }
+
+
+
+
+
+//funcion para contar el numero de caracteres de los campos 
 function updateCounter(fieldId, maxLength) {
     const field = document.getElementById(fieldId);
     const counter = document.getElementById(`${fieldId}-counter`);
@@ -5,9 +30,14 @@ function updateCounter(fieldId, maxLength) {
     counter.textContent = `${currentLength} / ${maxLength}`;
 }
 
+
+
+//funcion para mostrar contraseña
 const checkbox = document.getElementById('showPasswd');
         const passwordInput = document.getElementById('password');
 
     checkbox.addEventListener('change', () => {
         passwordInput.type = checkbox.checked ? 'text' : 'password';
     });
+
+
